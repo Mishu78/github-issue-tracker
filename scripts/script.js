b366aad0813f90018f6db53cef574242d35f79f5
programming hero <meherun2002@gmail.com>
2026-03-08 20:18:57 +0600

    data fetched and loaded

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,42 @@
-const loginBtn=document.getElementById('login-btn');
-loginBtn.addEventListener('click',function(){
-//1.get the username
-const inputName=document.getElementById("input-name");
-const username=inputName.value;
-console.log(username);
-//2.get the password
-const inputPassword=document.getElementById("input-password");
-const password=inputPassword.value; 
-console.log(password);
-//3.math username and password
-if(username=="admin" && password=="admin123"){
-    alert('login success');
 
-    window.location.assign("./home.html");
+
+//data load
+const issuesContainer=document.getElementById('issuesContainer');
+async function loadIssues(){
+    const res=await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    const data=await res.json();
+displayIssues(data.data);
 }
-else{
-    alert("login failed");
-    return;
-}
-//true::alert>homepage
-//false::alert>return
+
+function displayIssues(issues){
+console.log(issues);
+issues.forEach(issue => {
+    const card =document.createElement("div");
+    card.className="card w-96 bg-base-100 card-lg shadow-sm"
+    card.innerHTML=`
+     <div class="card-body">
+    <div class="flex justify-between">
+        <img src="./assets/Open-Status.png" alt="">
+        <div id="priority" class="badge badge-primary">High</div>
+    </div>
+    
+    <h2 id="issue-title" class="font-semibold text-lg mb-1">${issue.title}</h2>
+    <p id="issue-description" class="text-xs text-gray-500 mb-3 line-clamp-2">${issue.description}</p>
+    <h2 id="status"></h2>
+    <div class="flex gap-2 mb-3">
+        <span id="label" class="badge badge-error badge-outline text-xs ">${issue.labels}</span>
+        <span id="" class="badge badge-error badge-outline text-xs">Help Wanted</span>
+    </div>
+    <div class="text-xs text-gray-400 border-t pt-2">
+        <p>${issue.author}</p>
+        <p>${issue.createdAt}</p>
+    </div>
+  </div>
+  </div>
+    `
+    issuesContainer.appendChild(card);
+  
+    
 });
+}
+loadIssues();
